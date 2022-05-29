7600f4a2a6be742165b65c06d8d2050adfa65f31
Teddy <teddylnk1@gmail.com>
2022-05-29 16:56:05 +0300

    add unit test

---
+++ tests/test_loadData.py
@@ -0,0 +1,32 @@
+import unittest
+import pandas as pd
+import sys
+sys.path.append('../')
+from scripts.utils import Utils
+from scripts import log
+
+logger = log.setup_custom_logger(__name__, file_name='../logs/DataLoaderTest.log')
+utils = Utils()
+
+class TestDataLoader(unittest.TestCase):
+
+    def setUp(self) -> pd.DataFrame:
+        pass
+        
+
+    def test_read_csv(self):
+        ad_df = utils.load_data('tests/test_df.csv')
+        self.assertEqual(len(ad_df), 6)
+        logger.info("Reading local file successful")
+    
+    def test_dvc_get_data(self):
+        data_path = 'test/test_df.csv'
+        tag = 'v3-test'
+        repo = '.'
+        test_df = utils.load_data_dvc(tag,data_path, repo)
+        self.assertTrue(test_df['Open'].isnull().any())
+        logger.info("Reading data from dvc successfull")
+
+
+if __name__ == '__main__':
+    unittest.main()
