d710b0bba1ee3968a942afadc007e9e2ce3efbc7
Teddy <teddylnk1@gmail.com>
2022-05-29 18:11:47 +0300

    bug fix

--- tests/test_loadData.py
+++ tests/test_loadData.py
@@ -5,7 +5,7 @@ sys.path.append('../')
 from scripts.utils import Utils
 from scripts import log
 
-logger = log.setup_custom_logger(__name__, file_name='../logs/DataLoaderTest.log')
+# logger = log.setup_custom_logger(__name__, file_name='logs/DataLoaderTest.log')
 utils = Utils()
 
 class TestDataLoader(unittest.TestCase):
@@ -17,7 +17,7 @@ class TestDataLoader(unittest.TestCase):
     def test_read_csv(self):
         ad_df = utils.load_data('tests/test_df.csv')
         self.assertEqual(len(ad_df), 6)
-        logger.info("Reading local file successful")
+        print("Reading local file successful")
     
     def test_dvc_get_data(self):
         data_path = 'test/test_df.csv'
@@ -25,7 +25,7 @@ class TestDataLoader(unittest.TestCase):
         repo = '.'
         test_df = utils.load_data_dvc(tag,data_path, repo)
         self.assertTrue(test_df['Open'].isnull().any())
-        logger.info("Reading data from dvc successfull")
+        print("Reading data from dvc successfull")
 
 
 if __name__ == '__main__':
